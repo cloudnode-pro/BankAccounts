cbec0661d6d19d08458adfa170f435ec77b169b0
Zefir Kirilov <contact@zefir.pro>
2023-07-21 00:37:41 +0300

    allow null balance (makes it ∞)

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `bank_accounts` (
      `owner` CHAR(36) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
      `type` TINYINT NOT NULL COMMENT '0: personal, 1: business',
      `name` VARCHAR(24) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-     `balance` DECIMAL(15, 2) NOT NULL DEFAULT '0',
+     `balance` DECIMAL(15, 2),
      `frozen` TINYINT NOT NULL DEFAULT '0' COMMENT '0 (false) or 1 (true)',
      PRIMARY KEY (`id`)
 );
