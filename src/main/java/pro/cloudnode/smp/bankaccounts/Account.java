d6e0402df3a8a439b4610da28243339d74f593e4
Zefir Kirilov <contact@zefir.pro>
2023-12-28 18:57:51 +0200

    method to get server account

--- src/main/java/pro/cloudnode/smp/bankaccounts/Account.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/Account.java
@@ -279,6 +279,16 @@ public class Account {
     }
 
     /**
+     * Get the server account
+     */
+    public static @NotNull Optional<@NotNull Account> getServerAccount() {
+        if (!BankAccounts.getInstance().config().serverAccountEnabled()) return Optional.empty();
+        final @NotNull Account @NotNull [] accounts = get(BankAccounts.getConsoleOfflinePlayer());
+        if (accounts.length == 0) return Optional.empty();
+        return Optional.of(accounts[0]);
+    }
+
+    /**
      * Insert into database
      */
     public void insert() {
