0fc4bd553c81286644a47243369cfaeaaf2be817
Zefir Kirilov <contact@zefir.pro>
2023-12-28 19:00:48 +0200

    async bukkit timer for interest payments

--- src/main/java/pro/cloudnode/smp/bankaccounts/BankAccounts.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/BankAccounts.java
@@ -15,6 +15,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import pro.cloudnode.smp.bankaccounts.commands.BankCommand;
@@ -37,6 +38,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -152,6 +154,7 @@ public final class BankAccounts extends JavaPlugin {
                 getInstance().getLogger().warning("Update details: https://modrinth.com/plugin/bankaccounts/version/" + latestVersion);
             });
         });
+        getInstance().startInterestTimer();
     }
 
     /**
@@ -197,6 +200,44 @@ public final class BankAccounts extends JavaPlugin {
             getLogger().log(Level.SEVERE, "Could not initialize database.", e);
             getServer().getPluginManager().disablePlugin(this);
         }
+    }
+
+    private @Nullable BukkitTask interestTask = null;
+
+    /**
+     * Start interest timer
+     */
+    private void startInterestTimer() {
+        if (interestTask != null) interestTask.cancel();
+        interestTask = getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
+            final int currentMinutes = (int) Math.floor(System.currentTimeMillis() / 60000.0);
+            final double personalRate = config().interestRate(Account.Type.PERSONAL);
+            final int personalInterval = config().interestInterval(Account.Type.PERSONAL);
+            final double businessRate = config().interestRate(Account.Type.BUSINESS);
+            final int businessInterval = config().interestInterval(Account.Type.BUSINESS);
+            if ((personalInterval <= 0 && businessInterval <= 0) || (personalRate != 0 && businessRate != 0)) return;
+            final @NotNull Optional<@NotNull Account> serverAccount = Account.getServerAccount();
+            if (serverAccount.isEmpty() || serverAccount.get().frozen) return;
+            final @NotNull Account @NotNull [] accounts = Arrays.stream(Account.get()).filter(account -> !account.frozen && account.balance != null && account.balance.compareTo(BigDecimal.ZERO) > 0).toArray(Account[]::new);
+            if (personalInterval > 0 && personalRate != 0 && currentMinutes % personalInterval == 0) {
+                final @NotNull Account @NotNull [] personalAccounts = Arrays.stream(accounts).filter(account -> account.type == Account.Type.PERSONAL).toArray(Account[]::new);
+                for (final @NotNull Account account : personalAccounts) {
+                    assert account.balance != null;
+                    final @NotNull BigDecimal amount = account.balance.multiply(BigDecimal.valueOf(personalRate / 100.0)).abs().setScale(2, RoundingMode.DOWN);
+                    if (amount.compareTo(BigDecimal.ZERO) <= 0) continue;
+                    interestPayment(account, amount, personalRate, serverAccount.get());
+                }
+            }
+            if (businessInterval > 0 && businessRate != 0 && currentMinutes % businessInterval == 0) {
+                final @NotNull Account @NotNull [] businessAccounts = Arrays.stream(accounts).filter(account -> account.type == Account.Type.BUSINESS).toArray(Account[]::new);
+                for (final @NotNull Account account : businessAccounts) {
+                    assert account.balance != null;
+                    final @NotNull BigDecimal amount = account.balance.multiply(BigDecimal.valueOf(businessRate / 100.0)).abs().setScale(2, RoundingMode.DOWN);
+                    if (amount.compareTo(BigDecimal.ZERO) <= 0) continue;
+                    interestPayment(account, amount, businessRate, serverAccount.get());
+                }
+            }
+        }, 0L, 20L*60);
     }
 
     private void interestPayment(final @NotNull Account account, final @NotNull BigDecimal amount, final double rate, final @NotNull Account serverAccount) {
