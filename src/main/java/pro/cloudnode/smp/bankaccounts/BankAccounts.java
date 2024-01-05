d5818f25f1ed3ba730d02e1931cef14bc210a93a
Zefir Kirilov <contact@zefir.pro>
2024-01-05 06:08:01 +0200

    remove unused import

--- src/main/java/pro/cloudnode/smp/bankaccounts/BankAccounts.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/BankAccounts.java
@@ -45,7 +45,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.logging.Level;
