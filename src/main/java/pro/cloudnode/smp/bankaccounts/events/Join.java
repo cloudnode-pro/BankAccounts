e9e2ac3222990e41e47de211aae897a7afff0871
Zefir Kirilov <contact@zefir.pro>
2024-01-26 11:19:18 +0200

    make join event async

--- src/main/java/pro/cloudnode/smp/bankaccounts/events/Join.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/events/Join.java
@@ -15,20 +15,22 @@ import java.util.Optional;
 public final class Join implements Listener {
     @EventHandler
     public void onPlayerJoin(final @NotNull PlayerJoinEvent event) {
-        final Player player = event.getPlayer();
-        final @NotNull Optional<@NotNull Double> startingBalance = BankAccounts.getInstance().config()
-                .startingBalance();
-        startingBalance.ifPresent(aDouble -> BankAccounts.getInstance().getServer().getScheduler()
-                .runTaskAsynchronously(BankAccounts.getInstance(), () -> {
-                    final @NotNull Account[] accounts = Account.get(player, Account.Type.PERSONAL);
-                    if (accounts.length == 0) {
-                        new Account(player, Account.Type.PERSONAL, null, BigDecimal.valueOf(aDouble), false).insert();
-                    }
-                }));
-        if (player.hasPermission(Permissions.NOTIFY_UPDATE)) {
-            BankAccounts.getInstance().getServer().getScheduler().runTaskLater(BankAccounts.getInstance(), () -> BankAccounts.checkForUpdates().ifPresent(latestVersion -> {
-                player.sendMessage(BankAccounts.getInstance().config().messagesUpdateAvailable(latestVersion));
-            }), 20L);
-        }
+        BankAccounts.getInstance().getServer().getScheduler().runTaskAsynchronously(BankAccounts.getInstance(), () -> {
+            final Player player = event.getPlayer();
+            final @NotNull Optional<@NotNull Double> startingBalance = BankAccounts.getInstance().config()
+                    .startingBalance();
+            startingBalance.ifPresent(aDouble -> BankAccounts.getInstance().getServer().getScheduler()
+                    .runTaskAsynchronously(BankAccounts.getInstance(), () -> {
+                        final @NotNull Account[] accounts = Account.get(player, Account.Type.PERSONAL);
+                        if (accounts.length == 0) {
+                            new Account(player, Account.Type.PERSONAL, null, BigDecimal.valueOf(aDouble), false).insert();
+                        }
+                    }));
+            if (player.hasPermission(Permissions.NOTIFY_UPDATE)) {
+                BankAccounts.getInstance().getServer().getScheduler().runTaskLater(BankAccounts.getInstance(), () -> BankAccounts.checkForUpdates().ifPresent(latestVersion -> {
+                    player.sendMessage(BankAccounts.getInstance().config().messagesUpdateAvailable(latestVersion));
+                }), 20L);
+            }
+        });
     }
 }
