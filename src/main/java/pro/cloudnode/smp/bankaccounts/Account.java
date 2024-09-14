02bd7310c35426694cb066febcb6b9c9c2b09210
Zefir Kirilov <contact@zefir.pro>
2024-09-14 15:43:44 +0300

    get server Vault account

--- src/main/java/pro/cloudnode/smp/bankaccounts/Account.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/Account.java
@@ -348,6 +348,14 @@ public class Account {
     }
 
     /**
+     * Get the server Vault account
+     */
+    public static @NotNull Optional<@NotNull Account> getServerVaultAccount() {
+        if (!BankAccounts.getInstance().config().serverAccountEnabled()) return Optional.empty();
+        return getVaultAccount(BankAccounts.getConsoleOfflinePlayer());
+    }
+
+    /**
      * Insert into database
      */
     public void insert() {
