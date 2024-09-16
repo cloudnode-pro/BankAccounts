0031341b9a761b0cfffda53321b551be25e0964c
Zefir Kirilov <contact@zefir.pro>
2024-09-16 15:57:16 +0300

    remove unused import

--- src/main/java/pro/cloudnode/smp/bankaccounts/BankAccounts.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/BankAccounts.java
@@ -40,7 +40,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
