a7dfa692e213773a9a6a2897088e477a7d1d1761
Zefir Kirilov <contact@zefir.pro>
2024-11-30 16:21:22 +0200

    improve POS player interact event handler

--- src/main/java/pro/cloudnode/smp/bankaccounts/events/PlayerInteract.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/events/PlayerInteract.java
@@ -3,6 +3,7 @@ package pro.cloudnode.smp.bankaccounts.events;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -19,38 +20,42 @@ import java.util.Optional;
 
 public final class PlayerInteract implements Listener {
     @EventHandler
-    public void onPlayerInteractEvent(final @NotNull PlayerInteractEvent event) {
+    public void openPOS(final @NotNull PlayerInteractEvent event) {
         final @NotNull Player player = event.getPlayer();
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            final @NotNull Optional<Block> block = Optional.ofNullable(event.getClickedBlock());
-            if (block.isPresent() && block.get().getState() instanceof final @NotNull Chest chest && !chest.getInventory().isEmpty() && !(chest.getInventory() instanceof DoubleChestInventory)) {
-                final @NotNull Optional<POS> pos = POS.get(block.get());
-                if (pos.isEmpty()) return;
-                event.setCancelled(true);
-                if (player.getUniqueId().equals(pos.get().seller.owner.getUniqueId())) {
-                    POS.openOwnerGui(player, chest, pos.get());
-                    return;
-                }
-                if (!player.hasPermission(Permissions.POS_USE)) {
-                    player.sendMessage(BankAccounts.getInstance().config().messagesErrorsPosNoPermission());
-                    return;
-                }
-                final @NotNull ItemStack heldItem = player.getInventory().getItemInMainHand();
-                if (heldItem.getType() != BankAccounts.getInstance().config().instrumentsMaterial()) {
-                    player.sendMessage(BankAccounts.getInstance().config().messagesErrorsNoCard());
-                    return;
-                }
-                final @NotNull Optional<@NotNull Account> account = Account.get(heldItem);
-                if (account.isEmpty())
-                    player.sendMessage(BankAccounts.getInstance().config().messagesErrorsPosInvalidCard());
-                else {
-                    if (!player.hasPermission(Permissions.POS_USE_OTHER) && !account.get().owner.getUniqueId().equals(player.getUniqueId())) {
-                        player.sendMessage(BankAccounts.getInstance().config().messagesErrorsNotAccountOwner());
-                        return;
-                    }
-                    POS.openBuyGui(player, chest, pos.get(), account.get());
-                }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        final @NotNull Optional<Block> block = Optional.ofNullable(event.getClickedBlock());
+        if (block.isEmpty()) return;
+        if (!(block.get().getState() instanceof final @NotNull Chest chest)) return;
+        if (chest.getInventory().isEmpty()) return;
+        if (chest.getInventory() instanceof DoubleChestInventory) return;
+
+        final @NotNull Optional<POS> pos = POS.get(block.get());
+        if (pos.isEmpty()) return;
+
+        event.setUseInteractedBlock(Event.Result.DENY);
+
+        if (player.getUniqueId().equals(pos.get().seller.owner.getUniqueId())) {
+            POS.openOwnerGui(player, chest, pos.get());
+            return;
+        }
+        if (!player.hasPermission(Permissions.POS_USE)) {
+            player.sendMessage(BankAccounts.getInstance().config().messagesErrorsPosNoPermission());
+            return;
+        }
+        final @NotNull ItemStack heldItem = player.getInventory().getItemInMainHand();
+        if (heldItem.getType() != BankAccounts.getInstance().config().instrumentsMaterial()) {
+            player.sendMessage(BankAccounts.getInstance().config().messagesErrorsNoCard());
+            return;
+        }
+        final @NotNull Optional<@NotNull Account> account = Account.get(heldItem);
+        if (account.isEmpty()) player.sendMessage(BankAccounts.getInstance().config().messagesErrorsPosInvalidCard());
+        else {
+            if (!player.hasPermission(Permissions.POS_USE_OTHER) && !account.get().owner.getUniqueId()
+                    .equals(player.getUniqueId())) {
+                player.sendMessage(BankAccounts.getInstance().config().messagesErrorsNotAccountOwner());
+                return;
             }
+            POS.openBuyGui(player, chest, pos.get(), account.get());
         }
     }
 }
