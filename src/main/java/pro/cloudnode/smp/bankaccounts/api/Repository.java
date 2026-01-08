d7058c0a9fba215fcdfe978339c6f8b10458882c
Zefir Kirilov <contact@zefir.pro>
2026-01-08 10:02:31 +0200

    add copyright notice

--- src/main/java/pro/cloudnode/smp/bankaccounts/api/Repository.java
+++ src/main/java/pro/cloudnode/smp/bankaccounts/api/Repository.java
@@ -1,3 +1,18 @@
+/*
+ * A Minecraft economy plugin that enables players to hold multiple bank accounts.
+ * Copyright © 2023–2026 Cloudnode OÜ.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 package pro.cloudnode.smp.bankaccounts.api;
 
 import org.jetbrains.annotations.ApiStatus;
@@ -186,6 +201,7 @@ public abstract class Repository<T> {
     protected interface Binder {
         /**
          * Binds parameters to the statement.
+         *
          * @param stmt the statement to bind parameters to
          * @throws SQLException if an error occurs while binding parameters
          */
